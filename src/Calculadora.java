8d419ce53509740af651f78e55ddbd038bdeaf4e
Fernando2198 <fernando_frias@ciencias.unam.mx>
2023-11-22 21:43:24 -0600

    c

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -14,7 +14,7 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ErrorDeSintaxisException {
-        String cadena = "(1r4)*3-(1s2)+4*(1t6)";
+        String cadena = "(1r4)*3-(1s2)+4*(1t8)";
         comp= new Compilador();
         StringTokenizer lexemas = comp.analisisLexico(cadena);
         CompositeEA nodo = comp.arbolDeAnalisisSintactico(lexemas);
